ab89bad2043d70fc994e3bb8b6ccff73a1a6f02c
Justin Kelly <justinkelly@justinkelly.net>
2023-10-18 22:25:39 +0200

    added i18n

--- longlist2sql.pl
+++ longlist2sql.pl
@@ -15,39 +15,27 @@ my %longlist = %{$json->decode(join("", "@item"))};
 my @keys = keys(%longlist);
 
 my $inserts = {
-    'countries' => {
-        file => './out/countries.sql',
-        sql  => "INSERT INTO countries (co_id, co_continent_id, co_common_name, co_official_name, co_flag, co_tld, co_calling_codes, co_eu_member, co_enabled)\nVALUES\n"
-    },
-    'country_languages' => {
-        file => './out/country-languages.sql',
-        sql  => "INSERT INTO country_languages (cl_country_id, cl_language_id)\nVALUES\n"
-    },
-    'country_currencies' => {
-        file => './out/country-currencies.sql',
-        sql  =>  "INSERT INTO country_currencies (cc_country_id, cc_currency_id)\nVALUES\n"
-    },
-    'languages' => {
-        file => './out/languages.sql', 
-        sql  => "INSERT INTO languages (lg_id, lg_name)\nVALUES\n", 
-    },
-    'currencies' => {
-        file => './out/currencies.sql',
-        sql  => "INSERT INTO currencies (ct_id, ct_name)\nVALUES\n", 
-    },
-    'continents' => {
-        file => './out/continents.sql',
-        sql  =>"INSERT INTO continents (ct_id, ct_name)\nVALUES\n", 
-    },
+    'countries'           => "INSERT INTO countries (co_id, co_continent_id, co_common_name, co_official_name, co_flag, co_tld, co_calling_codes, co_eu_member, co_enabled)\nVALUES\n",
+    'country_languages'   => "INSERT INTO country_languages (cl_country_id, cl_language_id)\nVALUES\n",
+    'country_currencies'  => "INSERT INTO country_currencies (cc_country_id, cc_currency_id)\nVALUES\n",
+    'languages'           => "INSERT INTO languages (lg_id, lg_name)\nVALUES\n",
+    'currencies'          => "INSERT INTO currencies (ct_id, ct_name)\nVALUES\n",
+    'continents'          => "INSERT INTO continents (ct_id, ct_name)\nVALUES\n"
 };
 
-my $countriesOut          = printOpen($inserts->{'countries'});
-my $countryLanguagesOut   = printOpen($inserts->{'country_languages'});
-my $countryCurrenciesOut  = printOpen($inserts->{'country_currencies'});
-
 my %languages;
+my $languagesText = $inserts->{'languages'};
 my %currencies;
+my $currenciesText = $inserts->{'currencies'};
 my %continents;
+my $continentsText = $inserts->{'continents'};
+my $countriesText         = $inserts->{countries};
+my $countryLanguagesText  = $inserts->{country_languages};
+my $languagesRef;
+my $countryCurrenciesText = $inserts->{country_currencies};
+my $currenciesRef;
+my $countryContinentsText = "";
+my $continentsRef;
 
 my $start = 0;
 for $key (sort @keys) {
@@ -60,7 +48,7 @@ for $key (sort @keys) {
 
     my @keysContinent = keys(%$continent);
     my $nameContinent = $keysContinent[0];
-   
+
     my $euMember     = 'false';
     my $tldStr       = '';
     my $callStr      = '';
@@ -69,92 +57,87 @@ for $key (sort @keys) {
     $tldStr          = substr $$tld[0], 1 if(ref $tld eq ref []);
     $callStr         = join(',', @$callingCode) if(ref $callingCode eq ref []);
     
-    %languages = printJoinTable (
+
+    ($languagesRef, $countryLanguagesText) = printJoinTableText (
         $longlist{$key}->{languages}, 
-        $countryLanguagesOut,
+        $countryLanguagesText,
         $start,
         $key,
         \%languages,
         sub {my ($key, $hash) = @_; $$hash{$key};}
     );
+    %languages = %$languagesRef;
 
-    %currencies = printJoinTable (
+    ($currenciesRef, $countryCurrenciesText) = printJoinTableText (
         $longlist{$key}->{currency},
-        $countryCurrenciesOut,
+        $countryCurrenciesText,
         $start,
         $key,
         \%currencies,
         sub {my ($key, $hash) = @_; $$hash{$key}->{iso_4217_name};}
     );
+    %currencies = %$currenciesRef;
 
-    %continents = printJoinTable (
-        $continent, 
-        0,
-        $start,
-        $key,
-        \%continents,
+    ($continentsRef, $countryContinentsText) = printJoinTableText (
+        $continent,
+        $countryContinentsText,
+        $start, 
+        $key, 
+        \%continents, 
         sub {my ($key, $hash) = @_; $$hash{$key};}
     );
-
+    %continents = %$continentsRef;
+    
     if($start > 0){
-        print $countriesOut ",\n";
+        $countriesText .= ",\n";
     }
     $start = 1;
-    print $countriesOut "('$key', '$nameContinent', '$commonName', '$officialName', '$flag', '$tldStr', '$callStr', $euMember)";
+    $countriesText .= "('$key', '$nameContinent', '$commonName', '$officialName', '$flag', '$tldStr', '$callStr', $euMember)";
 }
+$continentsText = printTableText($continentsText, \%continents);
+$languagesText  = printTableText($languagesText , \%languages);
+$currenciesText = printTableText($currenciesText , \%languages);
 
-printClose($countriesOut);
-printClose($countryLanguagesOut);
-printClose($countryCurrenciesOut);
+open($outFile, '>', './out/i18n.sql');
 
-printTable($inserts->{'languages'} , \%languages);
-printTable($inserts->{'currencies'}, \%currencies);
-printTable($inserts->{'continents'}, \%continents);
+print $outFile "$continentsText\n";
+print $outFile "$countriesText;\n";
+print $outFile "$languagesText\n";
+print $outFile "$currenciesText\n";
+print $outFile "$countryLanguagesText;\n";
+print $outFile "$countryCurrenciesText;\n";
 
-sub printJoinTable {
-    my ($hash, $fh, $start, $in, $out, $getVal) = @_;
+close($outFile);
+
+sub printJoinTableText {
+    my ($hash, $text, $start, $in, $out, $getVal) = @_;
     if(ref $hash eq ref {}) {
         my @keys = sort keys(%$hash);
         foreach $key (@keys) {
             $$out{$key} = &$getVal($key, $hash);
-            if($fh) {
-                if($start > 0) {
-                    print $fh ",\n";
-                }
-                print $fh "('$in', '$key')"  
+            if($start > 0) {
+                $text .= ",\n";
             }
+            $text .= "('$in', '$key')"  
         }
     }
-    return %$out;
+    return ($out, $text);
 }
 
-sub printTable {
-    my ($args, $hash) = @_;
+sub printTableText {
+    my ($text, $hash) = @_;
     my @keys = sort keys(%$hash);
     my $start = 0;
-    my $fh = printOpen($args);
     for $key (@keys) {
         my $name = quote($hash->{$key});
         if($start > 0) {
-            print $fh ",\n";
+            $text .=  ",\n";
         }
         $start = 1;
-        print $fh "('$key', '$name')";
+        $text .=  "('$key', '$name')";
     }
-    printClose($fh);
-}
-
-sub printOpen {
-    my ($args) = @_;
-    my $fh = new FileHandle(">$args->{file}");
-    print $fh "$args->{sql}";
-    return $fh;
-}
-
-sub printClose {
-    my ($fh) = @_;
-    print $fh ";\n";
-    close($fh);
+    $text .= ";\n";
+    return $text;
 }
 
 sub quote {
